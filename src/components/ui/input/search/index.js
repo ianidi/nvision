a4e40c3894fb41a72d2561226d5964553dcba8cb
Аниди Иван <l.anidi@protonmail.com>
2021-03-15 18:17:09 +0300

    Commit

---
+++ src/components/ui/input/search/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scoped.scss'
+
+export const Search = ({title, icon, style}) => {
+
+  return (
+    <React.Fragment>
+      <div className="button" style={{...style}}>
+        {title}
+        {icon ? <div className="icon">{icon}</div> : ""}
+      </div>
+
+      <style jsx>{`
+      
+        `}</style>
+    </React.Fragment>
+  );
+};
--- src/pages/employee/index.js
+++ src/pages/employee/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import { Excel } from '../../components/ui/button';
 import { ReactComponent as IconArrowNav } from '../../assets/icons/arrow_nav.svg';
 import { ReactComponent as IconArrowDownloadExcel } from '../../assets/icons/arrow_download_excel.svg';
-import './style.scss'
+import './style.scoped.scss'
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableKey: {
+    color: "#7F7F7F",
+    fontWeight: "normal",
+    fontSize: 14,
+    textAlign: "left",
+  },
+  tableCell: {
+    color: "#151515",
+    fontSize: 14,
+    textAlign: "left",
+  },
+});
+
+function createData(title, vendor, type, startDate, endDate, status) {
+  return { title, vendor, type, startDate, endDate, status };
+}
+
+const rows = [
+  createData("Специалист по MS Excel", "Microsoft", "Type", "Да", "01.09.2020", "Да", "31.08.2020", "Действует"),
+];
 
 export const Employee = () => {
   return <React.Fragment>
@@ -16,6 +51,45 @@ export const Employee = () => {
           <Excel title="Выгрузить в Excel" icon={<IconArrowDownloadExcel />} />
         </div>
       </div>
+
+      <div className="d-flex justify-content-space-between">
+        <div className="filter">
+          
+        </div>
+        <div className="list">
+          <div className="d-flex align-items-center">
+            <input />
+          </div>
+          <div>
+            <TableEmployee />
+          </div>
+        </div>
+      </div>
     </div>
   </React.Fragment>;
 };
+
+function TableEmployee() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer style={{marginTop: "10px", marginBottom: "20px", userSelect: "none"}}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.tableKey}>Фамилия, Имя, Отчество сотрудника</TableCell>
+            <TableCell className={classes.tableKey}>Подразделение</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id} className={classes.tableLine}>
+              <TableCell className={classes.tableCell} component="th" scope="row">{row.title}</TableCell>
+              <TableCell className={classes.tableCell}>{row.startDate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
