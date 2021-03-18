0dd170e3ecdac8616c8a42cd9bc7c01f3cccc142
Аниди Иван <l.anidi@protonmail.com>
2021-03-18 16:08:33 +0300

    Commit

--- src/pages/cert/index.js
+++ src/pages/cert/index.js
@@ -9,6 +9,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import Box from "@material-ui/core/Box";
 import { Link, useParams } from "react-router-dom";
@@ -54,6 +55,33 @@ function createData(title, vendor, type, startDate, endDate, status) {
 const rows = [
   createData("Специалист по MS Excel", "Microsoft", "Type", "Да", "01.09.2020", "Да", "31.08.2020", "Действует"),
 ];
+
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
 
 function Action() {
   return (
@@ -114,6 +142,67 @@ export const Cert = () => {
 function TableEmployee() {
   const classes = useStyles();
 
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+
+
   const PATH = "/cert";
   const ROWS_PER_PAGE = 15;
 
@@ -124,6 +213,14 @@ function TableEmployee() {
       <Table className={classes.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox
+                indeterminate={selected.length > 0 && selected.length < rows.length}
+                checked={rows.length > 0 && selected.length === rows.length}
+                onChange={handleSelectAllClick}
+                inputProps={{ 'aria-label': 'select all' }}
+              />
+            </TableCell>
             <TableCell className={classes.tableKey}>ФИО сотрудника</TableCell>
             <TableCell className={classes.tableKey}>Наименование</TableCell>
             <TableCell className={classes.tableKey}>Вендор</TableCell>
@@ -135,19 +232,47 @@ function TableEmployee() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id} className={classes.tableLine}>
-              <TableCell className={classes.tableCell} component="th" scope="row">{row.title}</TableCell>
-              <TableCell className={classes.tableCell}>{row.vendor}</TableCell>
-              <TableCell className={classes.tableCell}>{row.type}</TableCell>
-              <TableCell className={classes.tableCell}>{row.type}</TableCell>
-              <TableCell className={classes.tableCell}>{row.endDate}</TableCell>
-              <TableCell className={classes.tableCell}>{row.startDate}</TableCell>
-              <TableCell className={classes.tableCell}>{row.status}</TableCell>
-              <TableCell className={classes.tableCell}><Action /></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        {stableSort(rows, getComparator(order, orderBy))
+          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          .map((row, index) => {
+            const isItemSelected = isSelected(row.name);
+            const labelId = `enhanced-table-checkbox-${index}`;
+
+            return (
+              <TableRow
+                // hover
+                // onClick={(event) => handleClick(event, row.name)}
+                role="checkbox"
+                aria-checked={isItemSelected}
+                tabIndex={-1}
+                key={row.name}
+                selected={isItemSelected}
+              >
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={isItemSelected}
+                    inputProps={{ 'aria-labelledby': labelId }}
+                    onClick={(event) => handleClick(event, row.name)}
+                  />
+                </TableCell>
+                    <TableCell className={classes.tableCell} component="th" scope="row" padding="none">{row.title}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.vendor}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.type}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.type}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.endDate}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.startDate}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.status}</TableCell>
+                    <TableCell className={classes.tableCell}><Action /></TableCell>
+              </TableRow>
+            );
+          })}
+        {emptyRows > 0 && (
+          <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+            <TableCell colSpan={9} />
+          </TableRow>
+        )}
+      </TableBody>
+
         </Table>
         
         <Box
@@ -179,3 +304,19 @@ function TableEmployee() {
     </TableContainer>
   );
 }
+
+
+// <TableBody>
+// {rows.map((row) => (
+//   <TableRow key={row.id} className={classes.tableLine}>
+//     <TableCell className={classes.tableCell} component="th" scope="row">{row.title}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.vendor}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.type}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.type}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.endDate}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.startDate}</TableCell>
+//     <TableCell className={classes.tableCell}>{row.status}</TableCell>
+//     <TableCell className={classes.tableCell}><Action /></TableCell>
+//   </TableRow>
+// ))}
+// </TableBody>
