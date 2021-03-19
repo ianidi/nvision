78d4b24994901fb6e40a74fd71f32f6730bb3147
Аниди Иван <l.anidi@protonmail.com>
2021-03-19 16:00:50 +0300

    Commit

--- src/components/ui/modal/filter/filter.js
+++ src/components/ui/modal/filter/filter.js
@@ -3,7 +3,6 @@ import {matchSorter} from 'match-sorter'
 import {usePopper} from 'react-popper'
 import {useCombobox, useMultipleSelection} from 'downshift'
 import {useDeepCompareEffect} from 'react-use'
-import Highlighter from 'react-highlight-words'
 import cc from 'classcat'
 
 const fruits = [
@@ -36,7 +35,7 @@ function CreateablePicker(props) {
   const [inputItems, setInputItems] = React.useState(items)
   const disclosureRef = React.useRef(null)
   const popoverRef = React.useRef(null)
-  const {styles, attributes, forceUpdate} = usePopper(
+  const {attributes, forceUpdate} = usePopper(
     disclosureRef.current,
     popoverRef.current,
     {
@@ -173,43 +172,30 @@ function CreateablePicker(props) {
   const selectedItemValues = selectedItems.map((item) => item.value)
 
   return (
-    <div className="relative w-full">
-      <label
-        {...getLabelProps({
-          className: 'font-medium text-gray-700 text-xs mb-2 block',
-        })}
+    <React.Fragment>
+
+    {selectedItems.map((selectedItem, index) => (
+      <div
+        key={`selected-item-${index}`}
+        {...getSelectedItemProps({selectedItem, index})}
       >
-        Choose some fruits:
-      </label>
-      <div>
-        <div className="my-2">
-          {selectedItems.map((selectedItem, index) => (
-            <span
-              key={`selected-item-${index}`}
-              className="inline-flex items-center px-2 py-1 rounded-full text-xs font-medium leading-4 bg-indigo-100 text-indigo-800 focus:outline-none focus:shadow-outline mr-2"
-              {...getSelectedItemProps({selectedItem, index})}
-            >
-              {selectedItem.label}
-              <button
-                onClick={(e) => {
-                  e.stopPropagation()
-                  removeSelectedItem(selectedItem)
-                }}
-                type="button"
-                className="flex-shrink-0 ml-1 inline-flex text-indigo-500 focus:outline-none focus:text-indigo-700"
-                aria-label="Remove small badge"
-              >
-                &#10005;
-              </button>
-            </span>
-          ))}
+        {selectedItem.label}
+        <div
+          onClick={(e) => {
+            e.stopPropagation()
+            removeSelectedItem(selectedItem)
+          }}
+        >
+          x
         </div>
-        <div className="relative" {...getComboboxProps()}>
+      </div>
+    ))}
+
+
+        <div {...getComboboxProps()}>
           <input
             {...getInputProps(
               getDropdownProps({
-                className:
-                  'w-full p-2 text-sm focus:outline-none focus:shadow-outline rounded border border-gray-400',
                 placeholder,
                 onClick: isOpen ? () => {} : openMenu,
                 onFocus: isOpen ? () => {} : openMenu,
@@ -217,60 +203,24 @@ function CreateablePicker(props) {
               }),
             )}
           />
-          <div className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
-            <button
-              className="text-gray-600 px-3 h-full focus:outline-none focus:shadow-outline"
-              {...getToggleButtonProps()}
-              aria-label={'toggle menu'}
-            >
-              &#8595;
-            </button>
-          </div>
         </div>
         <div
-          style={styles.popper}
           {...attributes.popper}
-          {...getMenuProps({ref: popoverRef, className: ' w-full'})}
+          {...getMenuProps({ref: popoverRef})}
         >
-          <ul className="bg-white shadow-md">
-            {isOpen &&
-              inputItems.map((item, index) => (
-                <li
-                  className={cc({
-                    'p-2 text-sm bg-white border-b': true,
-                    'bg-gray-100': highlightedIndex === index,
-                  })}
+          <div>
+            {inputItems.map((item, index) => (
+                <div
                   key={`${item.value}${index}`}
                   {...getItemProps({item, index})}
                 >
-                  {isCreating ? (
-                    <p>
-                      <span>Create</span>{' '}
-                      <span className="font-medium bg-yellow-300 text-yellow-900">
-                        {item.label}
-                      </span>
-                    </p>
-                  ) : (
-                    <div className="flex items-center space-x-2">
-                      {selectedItemValues.includes(item.value) && (
-                        <span role="img" aria-label="Selected">
-                          ✅
-                        </span>
-                      )}
-                      <Highlighter
-                        autoEscape
-                        searchWords={[inputValue || '']}
-                        textToHighlight={itemRenderer(item)}
-                        highlightClassName="bg-yellow-300"
-                      ></Highlighter>
-                    </div>
-                  )}
-                </li>
+                {itemRenderer(item)}
+                      {selectedItemValues.includes(item.value) && (<div>✅</div>)}
+                </div>
               ))}
-          </ul>
+          </div>
         </div>
-      </div>
-    </div>
+    </React.Fragment>
   )
 }
 
