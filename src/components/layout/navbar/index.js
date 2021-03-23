89ef5da57938f3ad8b1be7a90b13d463f46d1b8c
Аниди Иван <l.anidi@protonmail.com>
2021-03-23 14:45:51 +0300

    Commit

--- src/components/layout/navbar/index.js
+++ src/components/layout/navbar/index.js
@@ -1,24 +1,33 @@
-import React from "react";
-import { useHistory, useRouteMatch } from "react-router-dom";
+import React, { useState, useLayoutEffect } from "react";
+import { useHistory, useRouteMatch, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { closeDrawer } from "../../../store/uiSlice";
-import { ReactComponent as IconBack } from '../../../assets/icons/back.svg';
+import { ReactComponent as IconBack } from "../../../assets/icons/back.svg";
 import "./style.scoped.scss";
 
 var classNames = require("classnames");
 
 export const Navbar = ({ drawer }) => {
+  const location = useLocation();
+
+  const [title, setTitle] = useState("");
+
+  useLayoutEffect(() => {
+    console.log(location.pathname);
+    setTitle(location.pathname);
+  }, [location.pathname]);
+
   return (
     <React.Fragment>
       <div className="nav">
         <div className="nav__back">
           <IconBack style={{ width: 8, height: 14, marginRight: 10 }} />
-          База сертификатов
+          <div>{title}</div>
         </div>
       </div>
     </React.Fragment>
   );
-}
+};
 
 function MenuLink({ icon, title, to, count }) {
   const history = useHistory();
@@ -41,19 +50,17 @@ function MenuLink({ icon, title, to, count }) {
       <div
         className={classNames({
           menuLink__container: true,
-          menuLink_active: isActive !== null
+          menuLink_active: isActive !== null,
         })}
         onClick={memuClickHandler}
       >
         <div
           className={classNames({
-            menuLink: true
+            menuLink: true,
           })}
         >
           <div className="icon">{icon}</div>
-          <div className="menu__title">
-            {title}
-          </div>
+          <div className="menu__title">{title}</div>
           {count && <div className="count">{count}</div>}
         </div>
       </div>
