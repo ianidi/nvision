35cd456cc1a9030e1e551999463d30fc07e94e7f
Аниди Иван <l.anidi@protonmail.com>
2021-03-23 08:36:52 +0300

    Commit

--- src/pages/guide/index.js
+++ src/pages/guide/index.js
@@ -1,38 +1,49 @@
-import React, { useMemo } from 'react';
-import { ReactComponent as IconArrow } from '../../assets/icons/arrow.svg';
-import './style.scss'
+import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as IconArrow } from "../../assets/icons/arrow.svg";
+import "./style.scss";
 
 export const Guide = () => {
-
   const items = useMemo(
     () => [
       {
-        title: 'Справочник “Вендоры”',
-        path: 'vendor',
+        title: "Справочник “Вендоры”",
+        path: "vendor",
       },
       {
-        title: 'Справочник “Типы сертификатов”',
-        path: 'vendor',
+        title: "Справочник “Типы сертификатов”",
+        path: "cert",
       },
       {
-        title: 'Справочник “Специальности”',
-        path: 'vendor',
+        title: "Справочник “Специальности”",
+        path: "specialty",
       },
       {
-        title: 'Справочник “Виды удостоверений”',
-        path: 'vendor',
+        title: "Справочник “Виды удостоверений”",
+        path: "cert",
       },
       {
-        title: 'Справочник “Типы учёных степеней”',
-        path: 'vendor',
+        title: "Справочник “Типы учёных степеней”",
+        path: "degree",
       },
     ],
     []
   );
 
-  return <React.Fragment>
-    <div className="items">
-      {items.map((item, index) => (<div key={index} className="item"><div>{item.title}</div><div><IconArrow /></div></div>))}
-    </div>
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className="items">
+        {items.map((item, index) => (
+          <Link key={index} to={`/guide/${item.path}`}>
+            <div className="item">
+              <div>{item.title}</div>
+              <div>
+                <IconArrow />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </React.Fragment>
+  );
 };
