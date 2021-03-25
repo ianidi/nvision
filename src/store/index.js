7d378a72858f998ccf4c6d0c11c2bccbe9763803
Аниди Иван <l.anidi@protonmail.com>
2021-03-25 16:56:08 +0300

    Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,23 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import modalReducer from './modalSlice';
-import memberReducer from './memberSlice';
-import uiReducer from './uiSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import modalReducer from "./modalSlice";
+import memberReducer from "./memberSlice";
+import uiReducer from "./uiSlice";
+import dataReducer from "./dataSlice";
 // import thunk from "redux-thunk";
 
 const reducers = combineReducers({
   modal: modalReducer,
   member: memberReducer,
   ui: uiReducer,
+  data: dataReducer,
 });
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['ui', 'router'],
+  blacklist: ["ui", "router", "data"],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
