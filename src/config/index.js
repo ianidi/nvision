7ae7a3622c2433075e10f104b7f2481b634df24d
Аниди Иван <l.anidi@protonmail.com>
2021-03-30 15:02:15 +0300

    Commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,42 @@
-export { pageTitle } from "./pageTitle";
 export const API_URL = "http://localhost:4000";
+
+export const pageTitle = {
+  "/report": "Отчётность",
+  "/profile": "Мой профиль",
+  "/employee": "Сотрудники",
+  "/cert": "База сертификатов",
+  "/guide": "Справочники",
+  "/guide/vendor": "Справочник “Вендоры”",
+  "/guide/cert": "Справочник “Типы сертификатов”",
+  "/guide/specialty": "Справочник “Специальности”",
+  "/guide/credential": "Справочник “Виды удостоверений”",
+  "/guide/degree": "Справочник “Типы учёных степеней”",
+  vendor: "Справочник “Вендоры”",
+  cert: "Справочник “Типы сертификатов”",
+  specialty: "Справочник “Специальности”",
+  credential: "Справочник “Виды удостоверений”",
+  degree: "Справочник “Типы учёных степеней”",
+};
+
+export const guide = [
+  {
+    title: "Справочник “Вендоры”",
+    path: "vendor",
+  },
+  {
+    title: "Справочник “Типы сертификатов”",
+    path: "cert",
+  },
+  {
+    title: "Справочник “Специальности”",
+    path: "specialty",
+  },
+  {
+    title: "Справочник “Виды удостоверений”",
+    path: "credential",
+  },
+  {
+    title: "Справочник “Типы учёных степеней”",
+    path: "degree",
+  },
+];
