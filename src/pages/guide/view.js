d0d612735dcd543dc95df6e266e438555aeb2293
Аниди Иван <l.anidi@protonmail.com>
2021-03-31 16:36:16 +0300

    Commit

--- src/pages/guide/view.js
+++ src/pages/guide/view.js
@@ -159,7 +159,7 @@ function TableGuide() {
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(50);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
